6b80aea52dd3f0b33d3e96a93afd7c0f1f9b3b92
dltmddus1998 <dltmddus1998@naver.com>
2022-08-31 11:19:28 +0900

    Feat: 게시글 끌어올리기 & 가격제안 API 개발

--- src/posts/post.resolver.ts
+++ src/posts/post.resolver.ts
@@ -1,10 +1,14 @@
+import { AcceptOfferedPriceDto } from './dto/acceptOfferedPrice.dto';
 import { ParseIntPipe, UsePipes, ValidationPipe } from '@nestjs/common';
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { User } from 'src/users/user.entity';
 import { CreatePostDto } from './dto/createPost.dto';
 import { SearchPostDto } from './dto/searchPost.dto';
 import { UpdatePostDto } from './dto/updatePost.dto';
+import { PullUpPostInputDto } from './dto/pullUpPostInput.dto';
+import { OfferPriceDto } from './dto/offerPrice.dto';
 import { Post } from './post.entity';
+import { PriceOffer } from './priceOffer.entity';
 import { PostService } from './post.service';
 
 @Resolver(() => Post)
@@ -28,15 +32,36 @@ export class PostResolver {
     return this.postService.deletePost(postId);
   }
 
-  @Query(() => Post, {name: 'post'})
+  @Query(() => Post, { name: 'post' })
   getPostById(@Args('postId', ParseIntPipe) postId: number) {
     return this.postService.getPostById(postId);
   }
 
-  @Query(() => [Post], {name: 'posts'})
+  @Query(() => [Post], { name: 'posts' })
   @UsePipes(ValidationPipe)
   getPosts(@Args('searchPostDto') searchPostDto: SearchPostDto) {
     console.log(searchPostDto);
     return this.postService.getPosts(searchPostDto);
+  }
+
+  // 게시글 끌올
+  @Mutation(() => Post)
+  @UsePipes(ValidationPipe)
+  async pullupPost(@Args('pullUpPostInputDto') pullUpPostInputDto: PullUpPostInputDto) {
+    return await this.postService.pullUpPost(pullUpPostInputDto);
+  }
+
+  // 가격 제안 to 판매자
+  @Mutation(() => PriceOffer)
+  @UsePipes(ValidationPipe)
+  async offerPriceToSeller(@Args('offerPriceDto') offerPriceDto: OfferPriceDto): Promise<PriceOffer> {
+    return await this.postService.offerPrice(offerPriceDto);
+  }
+
+  // 가격 제안 수락
+  @Mutation(() => PriceOffer)
+  @UsePipes(ValidationPipe)
+  async acceptOfferedPriceOfSeller(@Args('acceptOfferedPriceDto') acceptOfferPriceDto: AcceptOfferedPriceDto): Promise<PriceOffer> {
+    return await this.postService.acceptOfferedPrice(acceptOfferPriceDto);
   }
 }
--- src/posts/post.service.ts
+++ src/posts/post.service.ts
@@ -1,17 +1,23 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { User } from 'src/users/user.entity';
 import { CreatePostDto } from './dto/createPost.dto';
 import { SearchPostDto } from './dto/searchPost.dto';
 import { UpdatePostDto } from './dto/updatePost.dto';
+import { PullUpPostInputDto } from './dto/pullUpPostInput.dto';
+import { OfferPriceDto } from './dto/offerPrice.dto';
+import { AcceptOfferedPriceDto } from './dto/acceptOfferedPrice.dto';
 import { Post } from './post.entity';
+import { PriceOffer } from './priceOffer.entity';
 import { PostRepository } from './post.repository';
 
 @Injectable()
 export class PostService {
   constructor(
     @InjectRepository(PostRepository)
-    private postRepository: PostRepository
+    private postRepository: PostRepository,
+    private connection: Connection,
   ) {}
 
   async createPost(createPostDto: CreatePostDto): Promise<Post> {
@@ -30,7 +36,7 @@ export class PostService {
     if (result.affected === 0) {
       throw new NotFoundException(`postId가 ${postId}인 것을 찾을 수 없습니다.`);
     }
-    return "삭제되었습니다.";
+    return '삭제되었습니다.';
   }
 
   async getPostById(postId: number): Promise<Post> {
@@ -43,5 +49,59 @@ export class PostService {
 
   async getPosts(searchPostDto: SearchPostDto): Promise<Post[]> {
     return this.postRepository.getPosts(searchPostDto);
+  }
+
+  async pullUpPost(pullUpPostInputDto: PullUpPostInputDto) {
+    /**
+     * @ 코드작성자: 이승연
+     * @ 기능: 게시글 끌어올리기
+     * @ 1️⃣ POST 테이블을 기본적으로 updatedAt을 기준으로 (default: now() === createdAt) 정렬한다. -> 전체 조회 중 해당 사항 처리예정 (✔︎)
+     * @ 2️⃣ POST 테이블에서 끌어올리고자 하는 게시글의 updatedAt을 현재 날짜로 수정한다. ()
+     */
+    /**
+     * 1. postId로 해당 게시물 먼저 찾기
+     * 2. 게시물 찾은 후 updatedAt을 현재날짜로 수정하기
+     */
+    const { postId } = pullUpPostInputDto;
+    const found = await this.postRepository.findOne(postId);
+
+    if (!found) {
+      throw new NotFoundException(`postId가 ${postId}인 것을 찾을 수 없습니다.`);
+    }
+
+    this.postRepository.changePulled(postId);
+
+    return this.getPostById(postId);
+  }
+
+  async offerPrice(offerPriceDto: OfferPriceDto): Promise<PriceOffer> {
+    /**
+     * @ 코드작성자: 이승연
+     * @ 기능: 가격 제시
+     * @ 부가 설명: 구매 희망자 -> 판매자 가격 제시
+     * @ 1️⃣ 가격 제안 요청 to 판매자 (제안하고자 하는 가격, 판매자 PUT) ()
+     * @ 2️⃣ 판매자에게 가격 제안 알림 기능 ()
+     * @ 🅰️ 수락시 - isOfferedPrice = true & price = offeredPrice로 재할당 ()
+     * @ 🅱️ 거절시 - nothing ()
+     */
+
+    const priceOfferedId = await this.postRepository.requestPriceToSeller(offerPriceDto);
+    console.log('?????');
+
+    // await this.postRepository.responsePriceToSeller(); // TODO - 유저 테이블 구체화된 후 수정 (알림기능)
+
+    const priceOffer = await PriceOffer.findOne({
+      where: {
+        priceOfferId: priceOfferedId,
+      },
+    });
+    console.log(priceOffer, 'here');
+    return priceOffer;
+  }
+
+  async acceptOfferedPrice(acceptOfferedPriceDto: AcceptOfferedPriceDto): Promise<PriceOffer> {
+    const priceOffered = await this.postRepository.determineOfferedPrice(acceptOfferedPriceDto);
+    console.log(priceOffered);
+    return priceOffered;
   }
 }
