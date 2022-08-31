886d14bf6a931ce693238a40fa9657b89ca99c53
dltmddus1998 <dltmddus1998@naver.com>
2022-08-31 11:16:51 +0900

    Feat: app.module 구성 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostModule } from './posts/post.module';
-import { typeORMConfig } from './configs/typeorm.config';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {PostModule} from './posts/post.module';
+import {typeORMConfig} from './configs/typeorm.config';
+import {GraphQLModule} from '@nestjs/graphql';
+import {ApolloDriver, ApolloDriverConfig} from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -14,7 +14,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
       debug: true,
       playground: true,
     }),
-    PostModule
+    PostModule,
   ],
 })
 export class AppModule {}
