718eb5e2545b579c0943e4d2443638096250ed72
dltmddus1998 <dltmddus1998@naver.com>
2022-09-01 03:00:45 +0900

    Delete: 필요없는 dto 파일 삭제 - 게시글 끌올 기능 관련

--- src/posts/dto/pullUpPostInput.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { IsNumber } from 'class-validator';
-
-@InputType()
-export class PullUpPostInputDto {
-  @Field()
-  @IsNumber()
-  readonly postId: number;
-}
