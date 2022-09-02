28fe4b26cb8eacc7edc6df5ea63cbc4d3affcd09
hjy <heojungyeon.97@gmail.com>
2022-09-02 20:50:01 +0900

    Fix: 캐시 ttl, max 값 변경 및 환경 변수 설정

--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -3,9 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
 import { UserResolver } from './user.resolver';
 import { UserService } from './user.service';
+import * as config from 'config';
+
+const cacheConfig: any = config.get('cache');
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserRepository]), CacheModule.register({ ttl: 600, max: 1000 })],
+  imports: [TypeOrmModule.forFeature([UserRepository]), CacheModule.register({ ttl: cacheConfig.ttl, max: cacheConfig.max })],
   providers: [UserService, UserResolver],
 })
 export class UserModule {}
