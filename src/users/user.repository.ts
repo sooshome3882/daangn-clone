a0ea894b92a200d0d05cd72de0f3e35fc058e9d0
hjy <heojungyeon.97@gmail.com>
2022-09-07 01:46:31 +0900

    Feat: @UseGuards() 추가

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,9 +1,14 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
+import { ProfileUserDto } from './dto/profile.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async join(marketingInfoAgree: boolean, phoneNumber: string) {
     await getRepository(User).createQueryBuilder('User').insert().into(User).values({ marketingInfoAgree, phoneNumber }).execute();
+  }
+
+  async setProfile(userName: string, profileImage: string) {
+    //  await getRepository(User).createQueryBuilder('User').update(User).set({ userName, profileImage }).where('phoneNumber = :phoneNumber', { phoneNumber: phoneNumber }).execute();
   }
 }
