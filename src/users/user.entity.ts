bef718a01e172d2c3dce8eeffa81e91fd3f38709
dltmddus1998 <dltmddus1998@naver.com>
2022-09-15 13:38:42 +0900

    Fix: Mypage로 옮긴 코드들 제거

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,9 @@
 import { Field, ObjectType } from '@nestjs/graphql';
 import { Post } from 'src/posts/post.entity';
 import { PostsLikeRecord } from 'src/posts/postsLikeRecord.entity';
+import { PurchaseHistory } from 'src/mypage/purchaseHistory.entity';
 import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryColumn, UpdateDateColumn } from 'typeorm';
-import { Followings } from './followings.entity';
+import { Followings } from '../mypage/followings.entity';
 
 @Entity()
 @ObjectType()
@@ -46,15 +47,15 @@ export class User extends BaseEntity {
   @OneToMany(type => Post, post => post.user, { eager: false })
   posts!: Post[];
 
-  @OneToMany(type => Post, post => post.buyer, { eager: false })
-  buyPosts!: Post[];
-
   @OneToMany(type => PostsLikeRecord, postsLikeRecord => postsLikeRecord.post, { eager: false })
   postsLikeRecord!: PostsLikeRecord[];
 
   @OneToMany(type => Followings, followings => followings.followingUser, { eager: false })
+  followings!: Followings[];
+
+  @OneToMany(type => Followings, followings => followings.followerUser, { eager: false })
   followers!: Followings[];
 
-  @OneToMany(type => Followings, followings => followings.followingUser, { eager: false })
-  followings!: Followings[];
+  @OneToMany(type => PurchaseHistory, purchaseHistory => purchaseHistory.user, { eager: false })
+  purchaseHistory!: PurchaseHistory[];
 }
--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,7 +1,7 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { FollowDto } from './dto/follow.dto';
-import { Followings } from './followings.entity';
+import { FollowDto } from '../mypage/dto/follow.dto';
+import { Followings } from '../mypage/followings.entity';
 import { SearchPostDto } from 'src/posts/dto/searchPost.dto';
 
 @EntityRepository(User)
@@ -16,30 +16,5 @@ export class UserRepository extends Repository<User> {
 
   async setProfileImage(phoneNumber: string, profileImage: string) {
     await getRepository(User).createQueryBuilder('User').update(User).set({ profileImage }).where('phoneNumber = :phoneNumber', { phoneNumber: phoneNumber }).execute();
-  }
-
-  async updateMarketingInfo(phoneNumber: string, marketingInfoAgree: boolean) {
-    await getRepository(User).createQueryBuilder('User').update(User).set({ marketingInfoAgree }).where('phoneNumber = :phoneNumber', { phoneNumber }).execute();
-  }
-
-  async followUsers(user: User, followDto: FollowDto) {
-    const { followers } = followDto;
-    const query = await getRepository(Followings).createQueryBuilder('Followings').insert().into(Followings).values({ followingUser: user, followerUser: followers }).execute();
-    return query.raw.insertId;
-  }
-
-  async deleteFollowUsers(followingId: number) {
-    return await getRepository(Followings).createQueryBuilder('Followings').delete().from(Followings).where('followingId = :followingId', { followingId }).execute();
-  }
-
-  async seeFollowUsers(searchPostDto: SearchPostDto) {
-    const { perPage, page } = searchPostDto;
-    return await getRepository(Followings)
-      .createQueryBuilder()
-      .select('followings')
-      .orderBy('followings.createdAt', 'DESC')
-      .offset((page - 1) * perPage)
-      .limit(perPage)
-      .getMany();
   }
 }
