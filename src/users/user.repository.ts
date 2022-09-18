5ab96a689278873f15fc0fca63ff13e443af1182
hjy <heojungyeon.97@gmail.com>
2022-09-18 14:27:42 +0900

    Chore: 객체 리터럴 수정, 필요없는 import 제거

--- src/users/user.repository.ts
+++ src/users/user.repository.ts
@@ -1,8 +1,5 @@
 import { EntityRepository, getRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
-import { FollowDto } from '../mypage/dto/follow.dto';
-import { Followings } from '../mypage/followings.entity';
-import { SearchPostDto } from 'src/posts/dto/searchPost.dto';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
@@ -11,10 +8,10 @@ export class UserRepository extends Repository<User> {
   }
 
   async setProfileUserName(phoneNumber: string, userName: string) {
-    await getRepository(User).createQueryBuilder('User').update(User).set({ userName }).where('phoneNumber = :phoneNumber', { phoneNumber: phoneNumber }).execute();
+    await getRepository(User).createQueryBuilder('User').update(User).set({ userName }).where('phoneNumber = :phoneNumber', { phoneNumber }).execute();
   }
 
   async setProfileImage(phoneNumber: string, profileImage: string) {
-    await getRepository(User).createQueryBuilder('User').update(User).set({ profileImage }).where('phoneNumber = :phoneNumber', { phoneNumber: phoneNumber }).execute();
+    await getRepository(User).createQueryBuilder('User').update(User).set({ profileImage }).where('phoneNumber = :phoneNumber', { phoneNumber }).execute();
   }
 }
