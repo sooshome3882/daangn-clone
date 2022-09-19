91f3d155c95279a070997b4d9bc700593f18c088
hjy <heojungyeon.97@gmail.com>
2022-09-19 09:51:49 +0900

    Chore: user service 주석 추가

--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -48,6 +48,14 @@ export class UserService {
   ) {}
 
   async getAroundTownList(myLocationDto: MyLocationDto): Promise<string[]> {
+    /**
+     * 내 위치에서 가까운 주변 동네 불러오기
+     *
+     * @author 허정연(golgol22)
+     * @param {latitude, longitude, from, size} 위도, 경도, offset, limit
+     * @return {string[]} 내 위치에서 가까운 동네부터'시도 + 시군구 + 읍면동' 형태로 반환
+     * @throws {NotFoundException} 주변 지역에 대한 정보가 없을 때 예외처리
+     */
     const { latitude, longitude, from, size } = myLocationDto;
     const result = await this.esService.search({
       index: 'coordinate',
@@ -81,6 +89,14 @@ export class UserService {
   }
 
   async getSearchTownList(area: string): Promise<string[]> {
+    /**
+     * 동네이름으로 동네 검색
+     *
+     * @author 허정연(golgol22)
+     * @param {area} 검색하고 싶은 동네 이름
+     * @return {string[]} 동네 이름에 검색어를 포함하는 동네 목록 '시도 + 시군구 + 읍면동' 형태로 반환
+     * @throws {NotFoundException} 검색한 지역에 대한 정보가 없을 때 예외처리
+     */
     const result = await this.esService.search({
       index: 'coordinate',
       body: {
@@ -106,6 +122,15 @@ export class UserService {
   }
 
   async verifyExistInData(area: string) {
+    /**
+     * 입력한 동네가 데이터에 있는지 여부 확인
+     *
+     * @author 허정연(golgol22)
+     * @param {area} 확인하고 싶은 동네 정보
+     * @returns {siDo, siGunGu, eupMyeonDong} 동네 정보를 시도, 시군구, 읍면동으로 나누어 반환
+     * @throws {BadRequestException} 동네 정보를 시도, 시군구, 읍면동으로 나눌 수 없을 때 예외처리
+     * @throws {NotFoundException} 위치 데이터에서 해당 정보를 찾을 수 없을 때 예외처리
+     */
     const [siDo, siGunGu, eupMyeonDong] = area.split(' ');
     if (!(siDo && siGunGu && eupMyeonDong)) {
       throw new BadRequestException('지역 형식이 올바르지 않습니다.');
@@ -145,8 +170,15 @@ export class UserService {
   }
 
   async join(joinUserDto: JoinUserDto): Promise<string> {
+    /**
+     * 회원가입
+     *
+     * @author 허정연(golgol22)
+     * @param {area, marketingInfoAgree, phoneNumber, isCertified} 내 동네, 마케팅 동의여부, 핸드폰 번호, sms인증확인여부
+     * @return {string} access token 반환
+     */
     const { area, marketingInfoAgree, phoneNumber } = joinUserDto;
-    const found = await this.getUserByPhoneNumber({ phoneNumber });
+    const found = await this.getUserByPhoneNumber(phoneNumber);
     if (!found) {
       const { siDo, siGunGu, eupMyeonDong } = await this.verifyExistInData(area);
       await this.userRepository.joinTransaction(marketingInfoAgree, phoneNumber, siDo, siGunGu, eupMyeonDong);
@@ -157,8 +189,16 @@ export class UserService {
   }
 
   async login(loginUserDto: LoginUserDto): Promise<string> {
+    /**
+     * 로그인
+     *
+     * @author 허정연(golgol22)
+     * @param {phoneNumber, isCertified} 핸드폰번호, sms인증확인여부
+     * @return {string} access token 반환
+     * @throws {UnauthorizedException} 등록되지 않은 사용자에 대한 예외처리
+     */
     const phoneNumber = loginUserDto.phoneNumber;
-    const found = await this.getUserByPhoneNumber({ phoneNumber });
+    const found = await this.getUserByPhoneNumber(phoneNumber);
     if (!found) {
       throw new UnauthorizedException('회원가입을 해주세요');
     }
@@ -167,19 +207,24 @@ export class UserService {
     return accessToken;
   }
 
-  async getUserByPhoneNumber({ phoneNumber }: { phoneNumber: string }): Promise<User> {
-    return await this.userRepository.findOne({ where: { phoneNumber } });
-  }
-
-  async getUserById(userId: number): Promise<User> {
-    const found = await this.userRepository.findOne(userId);
-    if (!found) {
-      throw new NotFoundException(`userId ${userId}인 것을 찾을 수 없습니다.`);
-    }
-    return found;
+  async getUserByPhoneNumber(phoneNumber: string): Promise<User> {
+    /**
+     * 핸드폰 번호로 유저 조회
+     *
+     * @author 허정연(golgol22)
+     * @param {phoneNumber} 핸드폰번호
+     * @return {User} 해당하는 유저 반환
+     */
+    return await this.userRepository.findOne(phoneNumber);
   }
 
   makeSignitureForSMS(): string {
+    /**
+     * sms를 보내기 위한 시그니처 생성
+     *
+     * @author 허정연(golgol22)
+     * @return {string} signiture
+     */
     const message = [];
     const hmac = crypto.createHmac('sha256', SECRET_KEY);
     const timeStamp = Date.now().toString();
@@ -198,10 +243,24 @@ export class UserService {
   }
 
   makeRand6Num(): number {
+    /**
+     * sms에 보낼 인증번호 6자리 생성
+     *
+     * @author 허정연(golgol22)
+     * @return {number} 인증번호 6자리
+     */
     return Math.floor(Math.random() * 1000000);
   }
 
   async sendSMS(phoneNumber: string): Promise<string> {
+    /**
+     * 인증번호 메시지 보내기
+     *
+     * @author 허정연(golgol22)
+     * @param {phoneNumber} 핸드폰번호
+     * @return {string} 인증번호 반환
+     * @throws {InternalServerErrorException} 인증번호 sms보내기 실패시 예외처리
+     */
     await this.cacheManager.del(phoneNumber);
     const checkNumber = this.makeRand6Num().toString().padStart(6, '0');
     const body: SMS = {
@@ -229,18 +288,35 @@ export class UserService {
         return checkNumber;
       }
     } catch (err) {
-      console.log(err.response.data);
+      console.error(err.response.data);
       throw new InternalServerErrorException('인증 메시지를 보내는데 실패하였습니다.');
     }
   }
 
   async checkSMS(phoneNumber: string, inputNumber: string): Promise<string> {
+    /**
+     * 인증번호 확인
+     *
+     * @author 허정연(golgol22)
+     * @param {phoneNumber, inputNumber} 핸드폰번호, 사용자가 입력한 인증번호
+     * @return {string} 인증번호 확인 유무 메시지
+     */
     const storedNumber = (await this.cacheManager.get(phoneNumber)) as string;
     if (storedNumber === inputNumber) return '인증이 완료되었습니다.';
     return '인증번호가 올바르지않습니다.';
   }
 
   async setProfile(phoneNumber: string, profileUserDto: ProfileUserDto): Promise<User> {
+    /**
+     * 유저 프로필 설정
+     *
+     * @author 허정연(golgol22)
+     * @param {phoneNumber, userName, profileImage} 핸드폰번호, 사용자 이름, 프로필 이미지
+     * @return {User} 유저 반환
+     * @throws {ConflictException} 이미 사용중인 사용자이름일 때 예외처리
+     * @throws {BadRequestException} 회원가입 후 첫 사용자 이름 설정인 경우 반드시 이름을 설정해야한다는 예외처리
+     * @throws {InternalServerErrorException} 프로필 이미지 저장 실패할 때 예외처리
+     */
     const { userName, profileImage } = profileUserDto;
     if (userName) {
       const found = await this.userRepository.findOne({ where: { userName } });
@@ -251,7 +327,7 @@ export class UserService {
     }
     const user = await this.userRepository.findOne(phoneNumber);
     if (user.userName === null) {
-      throw new ConflictException(`처음 이름은 꼭 설정해야 합니다.`);
+      throw new BadRequestException(`처음 이름은 꼭 설정해야 합니다.`);
     }
 
     if (profileImage) {
@@ -268,17 +344,33 @@ export class UserService {
       }
       await this.userRepository.setProfileImage(phoneNumber, `./src/users/uploads/${newFileName}.png`);
     }
-    return await this.getUserByPhoneNumber({ phoneNumber });
+    return await this.getUserByPhoneNumber(phoneNumber);
   }
 
   async getMyTownList(user: User): Promise<Location[]> {
+    /**
+     * 유저가 설정한 동네 목록 반환
+     *
+     * @author 허정연(golgol22)
+     * @param {user} 로그인한 유저
+     * @return {Location[]} 저장된 위치 정보 반환
+     */
     return await getRepository(Location).find({ where: { user: user.phoneNumber } });
   }
 
   async updateTownSelection(user: User, eupMyeonDong: string): Promise<Location[]> {
+    /**
+     * 선택된 동네 수정
+     *
+     * @author 허정연(golgol22)
+     * @param {user, eupMyeonDong} 로그인한 유저, 변경한 읍면동 이름
+     * @return {Location[]} 저장된 위치 정보 반환
+     * @throws {BadRequestException} 이미 선택된 동네일 경우 예외처리
+     * @throws {InternalServerErrorException} 동네 선택 수정에 실패할 때 예외처리
+     */
     const location = await getRepository(Location).findOne({ where: { user: user.phoneNumber, eupMyeonDong: eupMyeonDong } });
     if (location.isSelected) {
-      throw new BadRequestException('이미 선택된 지역입니다.');
+      throw new BadRequestException('이미 선택된 동네입니다.');
     }
     await getConnection()
       .transaction(async (manager: EntityManager) => {
@@ -296,6 +388,16 @@ export class UserService {
   }
 
   async addTown(user: User, area: string): Promise<Location[]> {
+    /**
+     * 동네 추가
+     *
+     * @author 허정연(golgol22)
+     * @param {user, area} 로그인한 유저, 추가할 동네 정보
+     * @return {Location[]} 저장된 위치 정보 반환
+     * @throws {BadRequestException} 2개 넘게 동네를 저장하려고 할 때 예외처리
+     * @throws {ConflictException} 추가하려는 동네가 이미 추가된 동네일 때 예외처리
+     * @throws {InternalServerErrorException} 동네 추가에 실패할 때 예외처리
+     */
     const { siDo, siGunGu, eupMyeonDong } = await this.verifyExistInData(area);
     const count = await getRepository(Location).count({ where: { user: user.phoneNumber } });
     if (count === 2) {
@@ -320,6 +422,17 @@ export class UserService {
   }
 
   async deleteTown(user: User, deleteTownDto: DeleteTownDto): Promise<Location[]> {
+    /**
+     * 동네 삭제
+     *
+     * @author 허정연(golgol22)
+     * @param {user, deleteDupMyeonDong, addArea} 로그인한 유저, 삭제할 읍변동 정보, 추가할 동네 정보
+     * @return {Location[]} 저장된 위치 정보 반환
+     * @throws {BadRequestException} 1개 저장된 동네를 삭제하려고 하는데 추가할 동네정보를 입력하지 않았을 때 예외처리
+     * @throws {ConflictException} 추가하려는 동네가 이미 추가된 동네일 때 예외처리
+     * @throws {NotFoundException} 등록된 적 없는 동네를 삭제하려고 할 때 예외처리
+     * @throws {InternalServerErrorException} 동네 삭제에 실패할 때 예외처리
+     */
     const { deleteDupMyeonDong, addArea } = deleteTownDto;
     const [locations, count] = await getRepository(Location).findAndCount({ where: { user: user.phoneNumber } });
     await getConnection()
@@ -337,6 +450,7 @@ export class UserService {
         const location2 = await getRepository(Location).findOne({ where: { user: user.phoneNumber, eupMyeonDong: Not(Equal(deleteDupMyeonDong)) } });
         location2.isSelected = true;
         await manager.save(location2);
+
         const result = await manager.delete(Location, { user: user.phoneNumber, eupMyeonDong: deleteDupMyeonDong });
         if (result.affected === 0) {
           throw new NotFoundException(`등록된 적 없는 동네입니다.`);
