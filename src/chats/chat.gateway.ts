ea083adf84ac755b1aebc206fc9f8e32d0c5b6ba
dltmddus1998 <dltmddus1998@naver.com>
2022-09-23 23:21:29 +0900

    Fix: chat gateway 우선 삭제처리 - socket 관련

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -23,44 +23,4 @@ let createdChatRooms = ChatRoom.find().then(chatRoom => {
     origin: '*',
   },
 })
-export class ChatGateway {
-  //   @WebSocketServer() server: Server;
-  //   @WebSocketServer() nsp: Namespace;
-  //   constructor(private readonly chatService: ChatService) {}
-  //   @SubscribeMessage('checkAuth')
-  //   checkAuth(socket: Socket, next: any) {
-  //     const token = socket.handshake.auth.token;
-  //     if (!token) {
-  //       throw next(new AuthenticationError('로그인을 해주세요'));
-  //     }
-  //     jwt.verify(token, jwtConfig.secret, (err, decoded) => {
-  //       if (err) {
-  //         return next(new AuthenticationError('로그인을 해주세요'));
-  //       }
-  //       next();
-  //     });
-  //   }
-  //   @SubscribeMessage('create-room')
-  //   handleCreateRoom(
-  //     @ConnectedSocket() socket: Socket,
-  //     @MessageBody() roomName: string,
-  //   ) {
-  //   }
-  //   afterInit() {
-  //     console.log('웹소켓 서버 초기화 ✅');
-  //   }
-  //   handleConnection(@ConnectedSocket() socket: Socket) {
-  //     console.log(`${socket.id} 소켓 연결`);
-  //   }
-  //   handleDisconnect(@ConnectedSocket() socket: Socket) {
-  //     console.log(`${socket.id} 소켓 연결 ❌`);
-  //   }
-  //   @SubscribeMessage('chat')
-  //   handleMessage(
-  //     @ConnectedSocket() socket: Socket,
-  //     @MessageBody() chat: string
-  //   ) {
-  //     socket.broadcast.emit('chat', { userName: socket.id, chat });
-  //     return { userName: socket.id, chat };
-  //   }
-}
+export class ChatGateway {}
