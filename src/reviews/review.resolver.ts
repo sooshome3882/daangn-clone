bcf66d8a73ae83ad74bc859b12259a2d1ccbf837
hjy <heojungyeon.97@gmail.com>
2022-09-24 00:54:14 +0900

    Feat: mannerItem 데이터 조회

--- src/reviews/review.resolver.ts
+++ src/reviews/review.resolver.ts
@@ -1,16 +1,22 @@
-import { UsePipes, ValidationPipe } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { JwtAuthGuard } from 'src/users/guards/jwtAuth.guard';
 import { User } from 'src/users/user.entity';
 import { GetUser } from 'src/users/validations/getUser.decorator';
-import { CreateSellerReviewDto } from './dto/createSellerReview.dto';
+import { ReviewDto } from './dto/review.dto';
+import { MannerItem } from './entities/mannerItem.entity';
 import { SellerReview } from './entities/sellerReview.entity';
 import { ReviewService } from './review.service';
 
 @Resolver()
+@UseGuards(JwtAuthGuard)
 export class ReviewResolver {
   constructor(private readonly reviewService: ReviewService) {}
 
-  @Mutation(() => SellerReview)
+  // mannerItem 데이터 조회
+  @Query(() => [MannerItem])
   @UsePipes(ValidationPipe)
-  createSellerReview(@GetUser() user: User, @Args('createSellerReviewDto') createSellerReviewDto: CreateSellerReviewDto) {}
+  getMannerItemData(): Promise<MannerItem[]> {
+    return this.reviewService.getMannerItemData();
+  }
 }
--- src/reviews/review.service.ts
+++ src/reviews/review.service.ts
@@ -1,4 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, ForbiddenException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PurchaseHistory } from 'src/mypage/purchaseHistory.entity';
+import { User } from 'src/users/user.entity';
+import { EntityManager, getConnection, getRepository } from 'typeorm';
+import { ReviewDto } from './dto/review.dto';
+import { MannerItem } from './entities/mannerItem.entity';
+import { SellerReview } from './entities/sellerReview.entity';
+import { ReviewRepository } from './review.repository';
 
 @Injectable()
-export class ReviewService {}
+export class ReviewService {
+  constructor(
+    @InjectRepository(ReviewRepository)
+    private reviewRepository: ReviewRepository,
+  ) {}
+
+  async getMannerItemData(): Promise<MannerItem[]> {
+    /**
+     * 거래후기 작성 항목 중 mannerItem에 대한 static data 조회
+     *
+     * @author 허정연(golgol22)
+     * @return {MannerItem[]} 매너항목 목록 반환
+     */
+    return await getRepository(MannerItem).find();
+  }
+}
