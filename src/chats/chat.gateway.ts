5e21354c05d82f0f01d10c944903ffee063ede56
dltmddus1998 <dltmddus1998@naver.com>
2022-09-23 23:20:40 +0900

    Fix: chat gateway 우선 삭제처리 - socket 관련

--- src/chats/chat.gateway.ts
+++ src/chats/chat.gateway.ts
@@ -8,8 +8,14 @@ import { User } from 'src/users/user.entity';
 import { CreateChatRoomDto } from './dto/createChatRoom.dto';
 import jwt from 'jsonwebtoken';
 import * as config from 'config';
+import { ChatRoom } from './chatRoom.entity';
 
 const jwtConfig: any = config.get('jwt');
+
+// let createdChatRooms: string[] = [];
+let createdChatRooms = ChatRoom.find().then(chatRoom => {
+  return chatRoom;
+});
 
 @WebSocketGateway(3000, {
   namespace: 'chat',
@@ -17,46 +23,44 @@ const jwtConfig: any = config.get('jwt');
     origin: '*',
   },
 })
-export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
-  @WebSocketServer() server: Server;
-  @WebSocketServer() nsp: Namespace;
-  constructor(private readonly chatService: ChatService) {}
-
-  @SubscribeMessage('checkAuth')
-  checkAuth(socket: Socket, next: any) {
-    const token = socket.handshake.auth.token;
-    if (!token) {
-      throw next(new AuthenticationError('로그인을 해주세요'));
-    }
-    jwt.verify(token, jwtConfig.secret, (err, decoded) => {
-      if (err) {
-        return next(new AuthenticationError('로그인을 해주세요'));
-      }
-      next();
-    });
-  }
-
-  afterInit() {
-    this.nsp.adapter.on('create-room', room => {
-      console.log(`"Room:${room}"이 생성되었습니다.`);
-    });
-    this.nsp.adapter.on('join-room', (room, id) => {
-      console.log(`"Socket:${id}"이 "Room:${room}"에 참여하였습니다.`);
-    });
-    this.nsp.adapter.on('leave-room', (room, id) => {
-      console.log(`"Socket:${id}"이 "Room:${room}"에서 나갔습니다.`);
-    });
-    this.nsp.adapter.on('delete-room', roomName => {
-      console.log(`"Room:${roomName}"이 삭제되었습니다.`);
-    });
-    console.log('웹소켓 서버 초기화 ✅');
-  }
-
-  handleConnection(@ConnectedSocket() socket: Socket) {
-    console.log(`${socket.id} 소켓 연결`);
-  }
-
-  handleDisconnect(@ConnectedSocket() socket: Socket) {
-    console.log(`${socket.id} 소켓 연결 ❌`);
-  }
+export class ChatGateway {
+  //   @WebSocketServer() server: Server;
+  //   @WebSocketServer() nsp: Namespace;
+  //   constructor(private readonly chatService: ChatService) {}
+  //   @SubscribeMessage('checkAuth')
+  //   checkAuth(socket: Socket, next: any) {
+  //     const token = socket.handshake.auth.token;
+  //     if (!token) {
+  //       throw next(new AuthenticationError('로그인을 해주세요'));
+  //     }
+  //     jwt.verify(token, jwtConfig.secret, (err, decoded) => {
+  //       if (err) {
+  //         return next(new AuthenticationError('로그인을 해주세요'));
+  //       }
+  //       next();
+  //     });
+  //   }
+  //   @SubscribeMessage('create-room')
+  //   handleCreateRoom(
+  //     @ConnectedSocket() socket: Socket,
+  //     @MessageBody() roomName: string,
+  //   ) {
+  //   }
+  //   afterInit() {
+  //     console.log('웹소켓 서버 초기화 ✅');
+  //   }
+  //   handleConnection(@ConnectedSocket() socket: Socket) {
+  //     console.log(`${socket.id} 소켓 연결`);
+  //   }
+  //   handleDisconnect(@ConnectedSocket() socket: Socket) {
+  //     console.log(`${socket.id} 소켓 연결 ❌`);
+  //   }
+  //   @SubscribeMessage('chat')
+  //   handleMessage(
+  //     @ConnectedSocket() socket: Socket,
+  //     @MessageBody() chat: string
+  //   ) {
+  //     socket.broadcast.emit('chat', { userName: socket.id, chat });
+  //     return { userName: socket.id, chat };
+  //   }
 }
