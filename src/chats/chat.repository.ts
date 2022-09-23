2f38e10d36a7ef2b2a33b8130d2676d392ce2483
dltmddus1998 <dltmddus1998@naver.com>
2022-09-23 23:15:02 +0900

    Fix: 안쓰는 메서드 삭제

--- src/chats/chat.repository.ts
+++ src/chats/chat.repository.ts
@@ -8,32 +8,6 @@ import { CreateChatDto } from './dto/createChat.dto';
 
 @EntityRepository(ChatRoom)
 export class ChatRepository extends Repository<ChatRoom> {
-  async getChatListOfOneChatRoom(user: User, chatRoomId: number) {
-    return await getRepository(Chat)
-      .createQueryBuilder('chat')
-      .innerJoinAndSelect('chat.chatRoom', 'chatRoom')
-      .innerJoinAndSelect('chat.user', 'user')
-      .where('chatRoom.chatRoomId = :chatRoomId', { chatRoomId })
-      .andWhere('user = :user', { user })
-      .orderBy('chat.createdAt', 'ASC')
-      .getMany();
-  }
-
-  async getChatRoomByPostAndUser(user: User, postId: number) {
-    return await getRepository(ChatRoom)
-      .createQueryBuilder('chatRoom')
-      .innerJoinAndSelect('chatRoom.post', 'post')
-      .innerJoinAndSelect('chatRoom.user', 'user')
-      .where('post = : postId', { postId })
-      .andWhere('user = :userName', { userName: user.phoneNumber })
-      .orderBy('chatRoom.createdAt', 'DESC')
-      .getOne();
-  }
-
-  async getChatById(chatId: number) {
-    return await getRepository(Chat).createQueryBuilder('chat').innerJoinAndSelect('chat.chatRoom', 'chatRoom').innerJoinAndSelect('chat.user', 'user').where('chatId = :chatId', { chatId }).getMany();
-  }
-
   async createChatRoom(user: User, createChatRoomDto: CreateChatRoomDto) {
     const { post } = createChatRoomDto;
     const query = await getRepository(ChatRoom).createQueryBuilder('ChatRoom').insert().into(ChatRoom).values({ post, user }).execute();
