6c53f63fafcfaa9189ff5fe23eee44580fb19823
dltmddus1998 <dltmddus1998@naver.com>
2022-09-28 17:05:46 +0900

    Chore: 필요없는 부분 삭제

--- src/posts/dto/createPostsComplaints.dto.ts
+++ src/posts/dto/createPostsComplaints.dto.ts
@@ -1,5 +1,5 @@
 import { Field, InputType } from '@nestjs/graphql';
-import { IsBoolean, IsNotEmpty, IsNumber, IsString, Length } from 'class-validator';
+import { IsNotEmpty, IsNumber } from 'class-validator';
 import { Post } from '../post.entity';
 import { ComplaintReason } from 'src/complaintReasons/complaintReason.entity';
 import { ProcessState } from 'src/processStates/processState.entity';
@@ -20,9 +20,4 @@ export class CreatePostsComplaintsDto {
   @IsNotEmpty()
   @IsNumber()
   processState!: ProcessState;
-
-  //   @Field()
-  //   @IsNotEmpty()
-  //   @IsString()
-  //   complaintUserId!: number;
 }
